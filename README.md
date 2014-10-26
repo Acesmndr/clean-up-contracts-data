67ddb56f060eb9258af03200ff9391035739bbf4
acesmndr <acesmndr@gmail.com>
2014-10-27 01:02:33 +0545

    Documentation of the script

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ clean-up-contracts-data
 =======================
 
 Challenge task for internship at YIPL.
+
+I have used JavaScript for merging the given CSV files.
+I haven't used any JavaScript libraries other than jQuery for accessing,parsing and exporting the csv files.
+I have used regular expression and splitting for parsing the CSV files into arrays and perform required operations.
--- myAttempt.js
+++ myAttempt.js
@@ -1,29 +1,26 @@
-$.ajaxSetup({ mimeType: "text/plain" });
-var awards,csvContent;
-var  ta,tc;
-//var atemp=new Array(),ctemp=new Array();   
-var total=0;	
-var temp;			
-var contracts;
-var final=new Array();
+$.ajaxSetup({ mimeType: "text/plain" }); // to fix the syntax error shown in firefox during csv file import
+var awards,contracts; //variables to store raw fetched data
+var  ta,tc; 
+var total=0; // variable to store the total amount of closed contracts	
+var final=new Array(),csvContent; // variables to store the final merged data
 
-$.ajax({
+$.ajax({                  //ajax request to get the csv file
     type: "GET",
     url: "awards.csv",
     dataType: "text",
     success: function(data) {
-			while((data.match(/(".*),(.*")/)||[]).length!=0){
-				data=data.replace(/(".*),(.*")/,"$1 xxcommaxx $2");
+			while((data.match(/(".*),(.*")/)||[]).length!=0){     //To check if the received CSV file consists of commas within the data itself
+				data=data.replace(/(".*),(.*")/,"$1 xxcommaxx $2");  //replace comma(,) in the data with a temporary data so that the data won't be split into  half 
 			}
-			awards=data.split(/\n/);
+			awards=data.split(/\n/); //seperating rows of data 
 			var l=awards.length;
 			for(i=0;i<l;i++){
-				awards[i]=awards[i].split(',');
+				awards[i]=awards[i].split(','); //seperating columns of data
 				};
 			console.log(awards);
            		},
     error: function (request, status, error) {
-        		alert("Awards csv file export failed! Cause:"+request.responseText);
+        		alert("Awards csv file export failed! Cause:"+request.responseText); //if the csv file couldn't be imported error is displayed
     			}
 	});
 $.ajax({
@@ -45,68 +42,65 @@ $.ajax({
         		alert("Contracts csv file export failed! Cause:"+request.responseText);
     			}
 	});
-$( document ).ajaxStop(function() {
-
-var j=0,k=0;
-	   console.log(contracts);	
-	   final[0]=contracts[0];
-	   var temp=awards[0].slice(1);
-	   console.log(temp);
+$( document ).ajaxStop(function() {   //after both the csv files are loaded the following code is executed
+	   var j=0;
+	   final[0]=contracts[0];  //the header of the contracts is copied to the final data array
+	   var temp=awards[0].slice(1);    // the header of awards is copied neglecting the first column which is similar to that of contracts 
+	   console.log("temp",temp);
 	   temp.forEach(function(item){		
-		     final[0].push(item);
+		     final[0].push(item); //then it is added to the final data 
 		});
-		console.log(final);
+		//console.log(final);
 		var atemp=new Array(),ctemp=new Array();   
 		for(i=1;i<awards.length;i++){
-	    		atemp.push(awards[i].splice(0,1));
+	    		atemp.push(awards[i].splice(0,1));    //first column elements of awards are seperated from other columns and stored in seperate variables
 		}
 		for(i=1;i<contracts.length;i++){
-	    		ctemp.push(contracts[i].splice(0,1));
+	    		ctemp.push(contracts[i].splice(0,1)); 
 		}
-		var template=new Array(awards.length);
-		for(var i=1;i<contracts.length-1;i++){
-			console.log("j is",j,"& i is",i);
-			var ta=parseInt(atemp[j].toLocaleString().slice(14));
+		var template=new Array(awards.length); //template for adding to contracts which havent been awarded yet
+		for(var i=1;i<contracts.length-1;i++){  // for all the contracts the following is carried out
+			//console.log("j is",j,"& i is",i);  
+			var ta=parseInt(atemp[j].toLocaleString().slice(14));  //the last digits are sliced from the contract name for matching
 			var tc=parseInt(ctemp[i-1].toLocaleString().slice(14));
-			if(ta==tc){
-				console.log("equal");
-				final[i]=$.merge(atemp[j],contracts[i-1+1]);
+			if(ta==tc){                       // if the contractname is same
+				final[i]=$.merge(atemp[j],contracts[i-1+1]);   //then the contract and the award is merged
 				final[i]=$.merge(final[i],awards[j+1]);
-				if(contracts[i-1+1][0]=="Closed"){
+				if(contracts[i-1+1][0]=="Closed"){      //if the contract is awarded and contract is closed then total is incremented with award amount
 					total+=parseInt(awards[j+1][4]);
 				}
-				console.log(contracts[i-1+1][1],awards[j+1][4]);
+				//console.log(contracts[i-1+1][1],awards[j+1][4]);
 				j++;
-				}else{
+				}else{                   // if contract isn't awarded contract is merged with the empty template
 				final[i]=$.merge(ctemp[i-1],contracts[i-1+1]);
 				final[i]=$.merge(final[i],template);
 				}
 				};	
-	console.log("FINAL",final);
+	//console.log("FINAL",final);
 
 
-csvContent = "data:text/csv;charset=utf-8,";
-final.forEach(function(infoArray, index){
+csvContent = "data:text/csv;charset=utf-8,";      //the csv content type is declared
+final.forEach(function(infoArray, index){        //the array is converted into csv format
 
-   dataString = infoArray.join(",");
+   dataString = infoArray.join(",");                 
    csvContent += index < infoArray.length ? dataString+ "\n" : dataString;
 
 
 }); 
 
-while(csvContent.match("xxcommaxx")!=null){
+while(csvContent.match("xxcommaxx")!=null){   //the previously used temporary variable is replaced back into it's normal form i.e. comma(,)
 csvContent=csvContent.replace("xxcommaxx",",");
 }
-console.log(csvContent);
+//console.log(csvContent);
 var encodedUri = encodeURI(csvContent);
-//window.open(encodedUri,final.csv);
-var downloadLink = document.createElement("a");
+//window.open(encodedUri,final.csv);   this wasn't done as the output filename couldn't be changed with this method !
+var downloadLink = document.createElement("a");  
 downloadLink.href = encodedUri;
-downloadLink.download = "final.csv";
+downloadLink.download = "final.csv";   
 
 document.body.appendChild(downloadLink);
-downloadLink.click();
+downloadLink.click();                   //for exporting the csv file
 document.body.removeChild(downloadLink);
 //alert("Total Amount of Closed Contracts: "+total);
-$("#display").html("Total Amount of Closed Contracts: "+total);
+$("#display").html("Total Amount of Closed Contracts: "+total);    //displaying the total amount of closed contracts
 });
